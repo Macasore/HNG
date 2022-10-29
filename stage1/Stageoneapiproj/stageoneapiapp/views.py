646bb51fbc0e7224d231032bf47ac887e7113bf6
macanotmarker <macasorekingdavid@gmail.com>
2022-10-29 02:19:17 +0100

    feat: configure views

--- stage1/Stageoneapiproj/stageoneapiapp/views.py
+++ stage1/Stageoneapiproj/stageoneapiapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import routers, viewsets, serializers
+from Stageoneapiproj.serializers import apidataSerializer
+from stageoneapiapp.models import apidata
 
 # Create your views here.
+
+#viewsets
+class apidataViewSet(viewsets.ModelViewSet):
+    queryset = apidata.objects.all()
+    serializer_class = apidataSerializer
