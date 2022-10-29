68a2340c9dd6e82605072c03baabf1567e0b18c7
macanotmarker <macasorekingdavid@gmail.com>
2022-10-29 02:05:47 +0100

    feat: added the base model

--- stage1/Stageoneapiproj/stageoneapiapp/models.py
+++ stage1/Stageoneapiproj/stageoneapiapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class apidata(models.Model):
+    slackUsername = models.CharField(max_length=200)
+    backend = models.BooleanField(default=True)
+    age = models.IntegerField()
+    bio = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.slackUsername
